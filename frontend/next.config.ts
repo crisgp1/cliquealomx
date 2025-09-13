eca8a4651537ad8dc21c1b77616f8dcb9cb70891
crisgp1 <cristiangp2001@gmail.com>
2025-09-13 02:03:44 -0600

    feat: agregar configuración de imágenes en next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,7 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   turbopack: {
     root: '../'
-  }
+  },
+  images: {
+    dangerouslyAllowSVG: true,
+    contentDispositionType: 'attachment',
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+  },
 };
 
 export default nextConfig;
